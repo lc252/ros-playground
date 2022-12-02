ab7bee5996ed4b80ab28d06a328e5d31dea8e65c
Lachlan <lc252@uowmail.edu.au>
2022-12-02 15:02:10 +1100

    added a magic carpet function

--- src/pcl_gen/nodes/cloudgen2.py
+++ src/pcl_gen/nodes/cloudgen2.py
@@ -1,6 +1,7 @@
 import rospy
 import numpy as np
 from sensor_msgs.msg import PointCloud2, PointField
+import time
 
 def random_PointCloud2(n):
     # generate a sensor_msgs.PointCloud2 object with n random points in 1x1m cube
@@ -14,7 +15,7 @@ def random_PointCloud2(n):
     pc2.header.stamp = rospy.Time.now()
 
     # fields - as described by the documentation
-    fields = ['x', 'y', 'z' , 'r', 'g', 'b', 'intensity']
+    fields = ['x', 'y', 'z', 'r', 'g', 'b', 'intensity']
 
     size_float32 = np.dtype(np.float32).itemsize
 
@@ -25,7 +26,7 @@ def random_PointCloud2(n):
     #       datatypes then the iterator will not work, perhaps 
     #       define the fields in a dict
     #   count = 1 ( 1 object per field)
-    pc2.fields = [PointField(name=n, offset=i*size_float32, datatype=PointField.FLOAT32, count=1) for i, n in enumerate(fields)]
+    pc2.fields = [PointField(name=name, offset=i*size_float32, datatype=PointField.FLOAT32, count=1) for i, name in enumerate(fields)]
 
     # generate n random points with x,y,z,field_n, ...
     # 1D array with [x1,y1,z1, ... xn,yn,zn]
@@ -38,20 +39,72 @@ def random_PointCloud2(n):
     # one point has 3 float32, therefore 12 bytes req.
     pc2.point_step = len(pc2.fields) * size_float32
     
-    # num data points (1000) * num fields per point (3) * bytes per point (4)
+    # num data points (1000) * num fields per point (3)
     pc2.row_step = pc2.point_step * n 
     return pc2
+
+
+def magic_carpet(n):
+    # generate a sensor_msgs.PointCloud2 object that moves as a carpet
+    
+    pc2 = PointCloud2(
+        height=1,
+        width=n
+    )
+
+    pc2.header.frame_id = "point_frame"
+    pc2.header.stamp = rospy.Time.now()
+
+    # fields - as described by the documentation
+    fields = ['x', 'y', 'z']
+    size_float32 = np.dtype(np.float32).itemsize
+
+    # generator for pointfield with:
+    #   name = names in fields array
+    #   offset = field number * size of data in a field
+    #   datatype = float32 --> if different fields have different 
+    #       datatypes then the iterator will not work, perhaps 
+    #       define the fields in a dict
+    #   count = 1 ( 1 object per field)
+    pc2.fields = [PointField(name=name, offset=i*size_float32, datatype=PointField.FLOAT32, count=1) for i, name in enumerate(fields)]
+
+    # x and y points do not change
+    x = np.array([np.linspace(0,1,50) for _ in range(50)])
+    y = np.transpose(x)
+
+    x = np.ndarray.flatten(x)
+    y = np.ndarray.flatten(y)
+
+    z = np.sin(x * 2*np.pi*0.5 + time.time())*0.25 + np.sin(y * 2*np.pi*0.25 + time.time())*0.05 + 0.25 
+
+    points = []
+    for i in range(len(x)):
+        points.append(x[i])
+        points.append(y[i])
+        points.append(z[i])
+    points = np.array(points)
+    # represents the points in a binary blob as float32's
+    # float32's take up 32 bits = 4 bytes
+    pc2.data = points.astype(np.float32).tobytes()
+
+    # one point has 3 float32, therefore 12 bytes req.
+    pc2.point_step = len(pc2.fields) * size_float32
+    
+    # num data points (1000) * num fields per point (3)
+    pc2.row_step = pc2.point_step * n 
+    return pc2
+
 
 
 def cloudgen():
     # publish a sensor_msgs.PointCloud2 msg to topic pt_cloud_gen @ 1Hz 
     pub = rospy.Publisher("PointCloud2", PointCloud2, queue_size=1)
     rospy.init_node("pt_cloud_gen", anonymous=True)
-    rate = rospy.Rate(1)
+    rate = rospy.Rate(24)
 
     while not rospy.is_shutdown():
 
-        pub.publish(random_PointCloud2(1000))
+        pub.publish(magic_carpet(50*50))
 
         rate.sleep()
         
