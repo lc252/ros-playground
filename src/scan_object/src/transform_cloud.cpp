2ab8f5aee3aa6975c9c8e8bb39bf8b9ae8be4a23
lc252 <lc252@uowmail.edu.au>
2022-12-16 16:59:13 +1100

    changed frame

--- src/scan_object/src/transform_cloud.cpp
+++ src/scan_object/src/transform_cloud.cpp
@@ -24,33 +24,36 @@ void transform_cloud(tf::StampedTransform camera_transform)
     tf::Vector3 t_m = camera_transform.getOrigin(); 
     tf::Quaternion r_m = camera_transform.getRotation();
 
+    double roll, pitch, yaw;
+    tf::Matrix3x3 (r_m).getRPY(roll, pitch, yaw);
+    std::cout << roll << " " << pitch << " " << yaw << std::endl;
+
     // construct a pcl transform
     Eigen::Affine3f transform = Eigen::Affine3f::Identity();
 
     // translate - x, y, z
     transform.translation() << t_m[0], t_m[1], t_m[2];
     
-    // rotate - roll, pitch, yaw
-    // must convert quarternion values to RPY for pcl tfs
-    // roll (x-axis rotation)
-    double sinr_cosp = 2 * (r_m[3] * r_m[0] + r_m[1] * r_m[2]);
-    double cosr_cosp = 1 - 2 * (r_m[0] * r_m[0] + r_m[1] * r_m[1]);
-    double roll = std::atan2(sinr_cosp, cosr_cosp);
-    // pitch (y-axis rotation)
-    double pitch = std::asin(2 * (r_m[3] * r_m[1] - r_m[2] * r_m[0]));
-    // yaw (z-axis rotation)
-    double siny_cosp = 2 * (r_m[3] * r_m[2] + r_m[0] * r_m[1]);
-    double cosy_cosp = 1 - 2 * (r_m[1] * r_m[1] + r_m[2] * r_m[2]);
-    double yaw = std::atan2(siny_cosp, cosy_cosp);
 
-    // taking the tf base_link->camera_link, manually tf'ing camera_link->camera_dept_optical_frame
-    transform.rotate(Eigen::AngleAxisf(roll - 1.571*2, Eigen::Vector3f::UnitX()));
-    transform.rotate(Eigen::AngleAxisf(pitch + 1.571, Eigen::Vector3f::UnitY()));
-    transform.rotate(Eigen::AngleAxisf(yaw + 1.571, Eigen::Vector3f::UnitZ()));
+    // taking the tf base_link->camera_link
+    transform.rotate(Eigen::AngleAxisf(roll, Eigen::Vector3f::UnitX()));
+    transform.rotate(Eigen::AngleAxisf(pitch, Eigen::Vector3f::UnitY())); //yaw
+    transform.rotate(Eigen::AngleAxisf(yaw, Eigen::Vector3f::UnitZ())); //roll
     
     // execute transform
     pcl::PointCloud<pcl::PointXYZRGB> transformed_cloud;
     pcl::transformPointCloud(cloud, transformed_cloud, transform);
+
+    // manual
+    // translate - x, y, z
+    transform.translation() << 0, 0, 0;
+    
+    // taking the tf base_link->camera_link, manually tf'ing camera_link->camera_dept_optical_frame
+    transform.rotate(Eigen::AngleAxisf(0, Eigen::Vector3f::UnitX()));
+    transform.rotate(Eigen::AngleAxisf(-1.571, Eigen::Vector3f::UnitY()));
+    transform.rotate(Eigen::AngleAxisf(0, Eigen::Vector3f::UnitZ())); //roll
+
+    pcl::transformPointCloud(transformed_cloud, transformed_cloud, transform);
 
     // create and publish ROS PointCloud2
     sensor_msgs::PointCloud2 ros_cloud;
@@ -90,7 +93,7 @@ int main(int argc, char** argv)
 
         // get the latest camera transform      
         tf_listener.waitForTransform("/camera_link", "/base_link", ros::Time::now() - delay, timeout);
-        tf_listener.lookupTransform("/map", "/camera_link", ros::Time::now() - delay, camera_transform);
+        tf_listener.lookupTransform("/base_link", "/camera_depth_optical_frame", ros::Time::now() - delay, camera_transform);
         transform_cloud(camera_transform);
         std::cout << "Cloud sent. " << std::endl;
     }
