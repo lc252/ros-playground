ebcf26c69fbd473c20e10a6bf85c827fb389244a
lc252 <lc252@uowmail.edu.au>
2023-01-18 09:47:16 +1100

    fixed indentation

--- src/pcl_filter_tutorial/src/voxel_example.cpp
+++ src/pcl_filter_tutorial/src/voxel_example.cpp
@@ -10,41 +10,41 @@ ros::Publisher pub;
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-  // Container for original & filtered data
-  pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-  pcl::PCLPointCloud2 cloud_filtered;
+    // Container for original & filtered data
+    pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+    pcl::PCLPointCloud2 cloud_filtered;
 
-  // Convert to PCL data type
-  pcl_conversions::toPCL(*cloud_msg, *cloud);
+    // Convert to PCL data type
+    pcl_conversions::toPCL(*cloud_msg, *cloud);
 
-  // Perform the actual filtering
-  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-  sor.setInputCloud (cloudPtr);
-  sor.setLeafSize (0.02, 0.02, 0.02);
-  sor.filter (cloud_filtered);
+    // Perform the actual filtering
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud (cloudPtr);
+    sor.setLeafSize (0.02, 0.02, 0.02);
+    sor.filter (cloud_filtered);
 
-  // Convert to ROS data type
-  sensor_msgs::PointCloud2 output;
-  pcl_conversions::fromPCL(cloud_filtered, output);
+    // Convert to ROS data type
+    sensor_msgs::PointCloud2 output;
+    pcl_conversions::fromPCL(cloud_filtered, output);
 
-  // Publish the data
-  pub.publish (output);
+    // Publish the data
+    pub.publish (output);
 }
 
 int main (int argc, char** argv)
 {
-  // Initialize ROS
-  ros::init(argc, argv, "voxel_example");
-  ros::NodeHandle nh;
+    // Initialize ROS
+    ros::init(argc, argv, "voxel_example");
+    ros::NodeHandle nh;
 
-  // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe("input", 1, cloud_cb);
+    // Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub = nh.subscribe("input", 1, cloud_cb);
 
-  // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2>("output_voxel", 1);
+    // Create a ROS publisher for the output point cloud
+    pub = nh.advertise<sensor_msgs::PointCloud2>("output_voxel", 1);
 
-  // Spin
-  ros::spin();
-  return 0;
+    // Spin
+    ros::spin();
+    return 0;
 }
