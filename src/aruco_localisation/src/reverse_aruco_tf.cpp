1325d77aadf1bc75ddcb365f9a0a391a7eecad62
lc252 <lc252@uowmail.edu.au>
2023-01-19 17:00:53 +1100

    took compute out of for loop - no more repeaty

--- src/aruco_localisation/src/reverse_aruco_tf.cpp
+++ src/aruco_localisation/src/reverse_aruco_tf.cpp
@@ -17,72 +17,62 @@ void inverse_transform(fiducial_msgs::FiducialTransformArray msg)
     // compute the inverse transform from camera to fiducial
 
     // first find the transform with the lowest error
-    fiducial_msgs::FiducialTransform inverse;
-    inverse.image_error = 999;
+    fiducial_msgs::FiducialTransform best_tf;
+    best_tf.image_error = 999;
     for(fiducial_msgs::FiducialTransform transform : msg.transforms)
     {
-        if(transform.image_error < inverse.image_error)
+        if(transform.image_error < best_tf.image_error)
         {
-            inverse = transform;
+            best_tf = transform;
         }
-
-        // Build transformation matrix
-        /* Description
-        Rotation matrix is 3x3 derived from quaternion, describes a unique rotational pose
-        Translation is a 3x1 vector
-
-        Transformation matrix
-        | R00 R01 R02 T00 |
-        | R10 R11 R12 T01 |
-        | R20 R21 R22 T02 |
-        |   0   0   0   1 |
-        */
-
-        Eigen::Quaternionf quat(
-            inverse.transform.rotation.w, 
-            inverse.transform.rotation.x, 
-            inverse.transform.rotation.y, 
-            inverse.transform.rotation.z);
-        Eigen::Vector3f tran(
-            inverse.transform.translation.x, 
-            inverse.transform.translation.y, 
-            inverse.transform.translation.z);
-
-        // identity matrix
-        Eigen::Matrix4f tf_matrix = Eigen::Matrix4f::Identity();
-        // insert rotation
-        tf_matrix.block<3,3>(0,0) = quat.toRotationMatrix();
-        // insert translation
-        tf_matrix.block<3,1>(0,3) = tran;
-
-        // inverse transformation matrix
-        Eigen::Matrix4f inv_tf = tf_matrix.inverse();
-        Eigen::Matrix3f m3 = inv_tf.block<3,3>(0,0);
-        // retrieve quat and tran for inversed matrix
-        Eigen::Quaternionf inv_quat(inv_tf.block<3,3>(0,0));
-        Eigen::Vector3f inv_tran(inv_tf.block<3,1>(0,3));
-
-        // Broadcaster
-        static tf::TransformBroadcaster br;
-
-        // tf to be broadcasted
-        geometry_msgs::TransformStamped msg;
-        msg.header.stamp = ros::Time::now();
-        msg.header.frame_id = "aruco_" + std::to_string(inverse.fiducial_id);
-        msg.child_frame_id = "camera_link";
-        // fill rotation values from Eigen
-        std::cout << inv_quat.x() << inv_quat.y() << inv_quat.z() << inv_quat.w() << std::endl;
-        msg.transform.rotation.x = inv_quat.x();
-        msg.transform.rotation.y = inv_quat.y();
-        msg.transform.rotation.z = inv_quat.z();
-        msg.transform.rotation.w = inv_quat.w();
-        // fill translation values from Eigen
-        msg.transform.translation.x = inv_tran.x();
-        msg.transform.translation.y = inv_tran.y();
-        msg.transform.translation.z = inv_tran.z();
-
-        br.sendTransform(msg);
     }
+
+    // extract quaternion and translation vector from the received fiducial_msgs::Fiducialransform
+    Eigen::Quaternionf quat(
+        best_tf.transform.rotation.w, 
+        best_tf.transform.rotation.x, 
+        best_tf.transform.rotation.y, 
+        best_tf.transform.rotation.z);
+    Eigen::Vector3f tran(
+        best_tf.transform.translation.x, 
+        best_tf.transform.translation.y, 
+        best_tf.transform.translation.z);
+
+    // Build transformation matrix
+    // identity matrix
+    Eigen::Matrix4f tf_matrix = Eigen::Matrix4f::Identity();
+    // insert rotation
+    tf_matrix.block<3,3>(0,0) = quat.toRotationMatrix();
+    // insert translation
+    tf_matrix.block<3,1>(0,3) = tran;
+
+    // inverse transformation matrix
+    Eigen::Matrix4f inv_tf = tf_matrix.inverse();
+    Eigen::Matrix3f m3 = inv_tf.block<3,3>(0,0);
+    // retrieve quat and tran for inversed matrix
+    Eigen::Quaternionf inv_quat(inv_tf.block<3,3>(0,0));
+    Eigen::Vector3f inv_tran(inv_tf.block<3,1>(0,3));
+
+    // Broadcaster
+    static tf::TransformBroadcaster br;
+
+    // tf to be broadcasted
+    geometry_msgs::TransformStamped tf_msg;
+    tf_msg.header.stamp = ros::Time::now();
+    tf_msg.header.frame_id = "aruco_" + std::to_string(best_tf.fiducial_id);
+    tf_msg.child_frame_id = "camera_link";
+    // fill rotation values from Eigen
+    std::cout << inv_quat.x() << inv_quat.y() << inv_quat.z() << inv_quat.w() << std::endl;
+    tf_msg.transform.rotation.x = inv_quat.x();
+    tf_msg.transform.rotation.y = inv_quat.y();
+    tf_msg.transform.rotation.z = inv_quat.z();
+    tf_msg.transform.rotation.w = inv_quat.w();
+    // fill translation values from Eigen
+    tf_msg.transform.translation.x = inv_tran.x();
+    tf_msg.transform.translation.y = inv_tran.y();
+    tf_msg.transform.translation.z = inv_tran.z();
+
+    br.sendTransform(tf_msg);
 
 }
 
