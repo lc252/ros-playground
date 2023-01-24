cbd5ab5f71aef195aabd68b22f3c2d412d49dba9
lc252 <lc252@uowmail.edu.au>
2023-01-24 17:13:25 +1100

    publishing camera info

--- src/depth_camera/src/realsense_camera.cpp
+++ src/depth_camera/src/realsense_camera.cpp
@@ -11,14 +11,22 @@
 #include <eigen3/Eigen/Eigen>
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <geometry_msgs/Transform.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <camera_info_manager/camera_info_manager.h>
 
 
 class RealSense_Camera
 {
 
+// Wrapper for an Intel Realsense Camera. This class improves the 
+// accessibility to configuring the camera stream for optimised results.
+
 public:
     RealSense_Camera()
     {
+        // constructor configures streams, starts the pipeline and
+        // publishes the static transforms
+
         // setup streams
         cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8);
         cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16);
@@ -33,11 +41,14 @@ public:
 
     void get_frames()
     {
+        // get the latest frames in the pipe
         frames = p.wait_for_frames();
     }
 
     sensor_msgs::ImagePtr get_rgb()
     {
+        // getter for 2D rgb image stream, returns a ros image
+        
         rs2::frame color_frame = frames.get_color_frame();
         // Create a cv::Mat object from the color frame
         cv::Mat mat = cv::Mat(cv::Size(640, 480), CV_8UC3, (void*)color_frame.get_data(), cv::Mat::AUTO_STEP);
@@ -69,6 +80,7 @@ public:
         return output_cloud;
     }
 
+
 private: 
     // pipeline and configuration for camera stream
     rs2::pipeline p;
@@ -82,6 +94,9 @@ private:
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr PCL_Conversion(const rs2::points& points)
     {
+        // convert rs2 points into a PCL pointcloud so that it can be serialised
+        // into a ROS message
+
         // pcl_cloud is a pointer to pcl cloud
         pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
@@ -111,12 +126,16 @@ private:
 
     void publish_static_tf()
     {
+        // There are extrinsic transforms between the frames of the camera
+        // that must be published to locate the point clouds in the world
+        // frame. 
+
         get_frames();
 
         // The base frame for the camera is analogous with the depth frame
         rs2::stream_profile base_frame = frames.get_depth_frame().get_profile();
         rs2::stream_profile color_frame = frames.get_color_frame().get_profile();
-        // get the tf from base to color frame. Realsense terminology is backwards.
+        // get the tf from base_frame to color_frame. Realsense terminology is backwards.
         rs2_extrinsics ext = color_frame.get_extrinsics_to(base_frame);
 
         
@@ -155,9 +174,40 @@ int main(int argc, char **argv)
     // image publisher
     image_transport::ImageTransport it(nh);
     image_transport::Publisher im_pub = it.advertise("camera/image", 1);
+    // camera info publisher
+    ros::Publisher info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera/info", 1);
+    camera_info_manager::CameraInfoManager info_manager(nh, "realsense");
+    // camera info should not change, only get once
+    sensor_msgs::CameraInfo info;
+    /*
+    header: 
+    seq: 354
+    stamp: 
+        secs: 1674540313
+        nsecs: 185359955
+    frame_id: "camera_color_optical_frame"
+    height: 720
+    width: 1280
+    distortion_model: "plumb_bob"
+    D: [0.0, 0.0, 0.0, 0.0, 0.0]
+    K: [923.98388671875, 0.0, 647.1097412109375, 0.0, 924.1301879882812, 369.11614990234375, 0.0, 0.0, 1.0]
+    R: [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+    P: [923.98388671875, 0.0, 647.1097412109375, 0.0, 0.0, 924.1301879882812, 369.11614990234375, 0.0, 0.0, 0.0, 1.0, 0.0]
+    binning_x: 0
+    binning_y: 0
+    roi: 
+    x_offset: 0
+    y_offset: 0
+    height: 0
+    width: 0
+    do_rectify: False
+    */
+    info.header.frame_id = "color_frame";
+    info.height = 480;
+    info.width = 640;
+    info.distortion_model = "plumb_bob";
 
-
-    // Camera
+    // init camera
     RealSense_Camera cam;
 
     // Publish the static tf's
@@ -174,6 +224,9 @@ int main(int argc, char **argv)
         // get the point cloud
         // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = cam.get_cloud();
         cloud_pub.publish(cam.get_cloud());
+
+        // publish camera info
+        info_pub.publish(info);
 
         // there is no ros::Rate() specified, therefore the speed is limited by the frame rate of the cameras
         ros::spinOnce();
