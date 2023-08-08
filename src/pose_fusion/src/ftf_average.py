5afa498216af560fa99b804c4594a01a5658feb4
lc252 <lc252@uowmail.edu.au>
2023-08-08 17:38:06 +1000

    added lookup expected tf

--- src/pose_fusion/src/ftf_average.py
+++ src/pose_fusion/src/ftf_average.py
@@ -3,8 +3,9 @@
 import rospy
 import numpy as np
 from fiducial_msgs.msg import FiducialTransform, FiducialTransformArray
-from geometry_msgs.msg import Vector3, Quaternion, PoseWithCovarianceStamped
-from tf import transformations, TransformListener
+from geometry_msgs.msg import Vector3, Quaternion, PoseWithCovarianceStamped, TransformStamped
+from scipy.spatial.transform import Rotation as R
+import tf2_ros as tf2
 
 
 
@@ -12,8 +13,8 @@ class ftf_average():
     def __init__(self):
         self.ftf_sub = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.transform_pose)
         self.pose_pub = rospy.Publisher(f"/pose/filtered", PoseWithCovarianceStamped, queue_size=1)
-        self.listener = TransformListener()
-        # self.seq = 0
+        self.buffer = tf2.Buffer()
+        self.listener = tf2.TransformListener(self.buffer)
 
     def conversion_cb(self, ftf_arr : FiducialTransformArray):
         pwcs = PoseWithCovarianceStamped()
@@ -29,7 +30,7 @@ class ftf_average():
 
         ftf : FiducialTransform
         for ftf in ftf_arr.transforms:
-            euler = transformations.euler_from_quaternion([ftf.transform.rotation.x, ftf.transform.rotation.y, ftf.transform.rotation.z, -ftf.transform.rotation.w,])
+            euler = R.from_quat([ftf.transform.rotation.x, ftf.transform.rotation.y, ftf.transform.rotation.z, ftf.transform.rotation.w]).as_euler("xyz")
             roll.append(euler[0])
             pitch.append(euler[1])
             yaw.append(euler[2])
@@ -45,10 +46,9 @@ class ftf_average():
                         [0, 0, 0, 0, np.var(pitch), 0],
                         [0, 0, 0, 0, 0, np.var(yaw)]])
 
-        q = transformations.quaternion_from_euler(np.mean(roll), np.mean(pitch), np.mean(yaw))
+        q = R.from_euler("xyz", [np.mean(roll), np.mean(pitch), np.mean(yaw)]).as_quat()
         pwcs.pose.pose.position = Vector3(np.mean(x), np.mean(y), np.mean(z))
         pwcs.pose.pose.orientation = Quaternion(*q)
-        # pwcs.pose.covariance = cov.flatten()
 
         # publish
         self.pose_pub.publish(pwcs)
@@ -57,7 +57,7 @@ class ftf_average():
     def transform_pose(self, ftf_arr : FiducialTransformArray):
         pwcs = PoseWithCovarianceStamped()
         pwcs.header.stamp = ftf_arr.header.stamp
-        pwcs.header.frame_id = f"base_link"
+        pwcs.header.frame_id = "base_link"
         
         x = []
         y = []
@@ -68,23 +68,45 @@ class ftf_average():
 
         ftf : FiducialTransform
         for ftf in ftf_arr.transforms:
-            # apply a transform back to the base link based on the fiducial id and known aruco positions
-            lookup_time = rospy.Time.now()
-            self.listener.waitForTransform(f"aruco_{ftf.fiducial_id}_known", f"base_link", lookup_time, rospy.Duration(0.1))
-            known_tf = self.listener.lookupTransform(f"aruco_{ftf.fiducial_id}_known", f"base_link", lookup_time)
+            # get the known base -> aruco transform
+            ar_base_TF : TransformStamped
+            ar_base_TF = self.buffer.lookup_transform("base_link", f"aruco_{ftf.fiducial_id}_known", rospy.Time())
+            # extract the translation and rotation
+            ar_base_t = ar_base_TF.transform.translation
+            ar_base_q = ar_base_TF.transform.rotation
+            ar_base_r = R.from_quat([ar_base_q.x, ar_base_q.y, ar_base_q.z, ar_base_q.w])
+            # build the transform matrix
+            ar_base_M = np.identity(4)
+            ar_base_M[0:3,0:3] = ar_base_r.as_matrix()
+            ar_base_M[0:3, 3] = np.array([ar_base_t.x, ar_base_t.y, ar_base_t.z])
 
-            # apply the transform to the fiducial transform
-            # ftf.transform = ftf.transform * known_tf
+            # get the camera -> aruco transform
+            cam_ar_t = ftf.transform.translation
+            cam_ar_q = ftf.transform.rotation
+            cam_ar_r = R.from_quat([cam_ar_q.x, cam_ar_q.y, cam_ar_q.z, cam_ar_q.w])
+            # build the transform matrix
+            cam_ar_M = np.identity(4)
+            cam_ar_M[0:3,0:3] = cam_ar_r.as_matrix()
+            cam_ar_M[0:3, 3] = np.array([cam_ar_t.x, cam_ar_t.y, cam_ar_t.z])
+
+            # get the camera -> base transform
+            cam_base_M = np.matmul(cam_ar_M, ar_base_M)
+            # extract the rotation matrix
+            cam_base_r = R.from_matrix(cam_base_M[0:3,0:3])
+            # extract the euler angles
+            euler = cam_base_r.as_euler("xyz")
+
+            # rospy.logerr(f"euler: {euler}, translation: {ftf.fiducial_id}")
             
-            euler = transformations.euler_from_quaternion([ftf.transform.rotation.x, ftf.transform.rotation.y, ftf.transform.rotation.z, -ftf.transform.rotation.w,])
+            # append rotation and translation to lists for averaging
             roll.append(euler[0])
             pitch.append(euler[1])
             yaw.append(euler[2])
+            x.append(cam_base_M[0,3])
+            y.append(cam_base_M[1,3])
+            z.append(cam_base_M[2,3])
 
-            x.append(-ftf.transform.translation.x)
-            y.append(-ftf.transform.translation.y)
-            z.append(-ftf.transform.translation.z)
-        
+        # calculate the covariance matrix
         cov = np.array([[np.var(x), 0, 0, 0, 0, 0],
                         [0, np.var(y), 0, 0, 0, 0],
                         [0, 0, np.var(z), 0, 0, 0],
@@ -92,15 +114,14 @@ class ftf_average():
                         [0, 0, 0, 0, np.var(pitch), 0],
                         [0, 0, 0, 0, 0, np.var(yaw)]])
 
-        q = transformations.quaternion_from_euler(np.mean(roll), np.mean(pitch), np.mean(yaw))
+        # calculate the average rotation and translation
+        q = R.from_euler("xyz", [np.mean(roll), np.mean(pitch), np.mean(yaw)]).as_quat()
         pwcs.pose.pose.position = Vector3(np.mean(x), np.mean(y), np.mean(z))
         pwcs.pose.pose.orientation = Quaternion(*q)
         # pwcs.pose.covariance = cov.flatten()
 
         # publish
         self.pose_pub.publish(pwcs)
-        # self.seq += 1
-
 
 
 if __name__ == "__main__":
